70b92d022a06f53df5cfb2a4a22c7fc6fb338838
Robert Bravery <rbravery@iqbusiness.net>
2017-03-11 11:20:27 +0200

    Refactored footer. bootstrap layout

--- js/mvbc.js
+++ js/mvbc.js
@@ -1,3 +1,6 @@
  function getYear() {
      $('#copyrightdate').html(new Date().getFullYear());
- };
+ }
+ $("#navbar").load("mainnav.html");
+ $("#headersection").load("header.html");
+ $("#footersection").load("footer.html");
